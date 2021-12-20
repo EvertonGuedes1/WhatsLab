28af813f1879193854410f36b507d080a533663b
Everton Guedes <evertonguedes1@gmail.com>
2021-12-20 18:07:31 -0300

    parte estática pronta

--- whatslabb/src/App.js
+++ whatslabb/src/App.js
@@ -3,17 +3,49 @@ import styled from "styled-components";
 
 const AppContainer = styled.div`
   border: 1px solid black;
-  height: 100vh;
+  height: 97vh;
   box-sizing: border-box;
-  width: 600px;
+  width: 98vw;
   margin: auto;
+  display: flex;
+  flex-direction: column;
+`
+
+const MessagesContainer = styled.div`  
+  flex-grow: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+`
+const InputsContainer = styled.div`
+  display: flex;
+`
+
+const NameInput = styled.input`
+  width: 100px;
+`
+
+const MessageInput = styled.input`
+  flex-grow: 1;
 `
 
 class App extends React.Component {
   render() {
     return (
       <AppContainer>
-        Olar Whatslabbbbbb
+        <MessagesContainer>
+          <p>
+            <strong>Maria</strong>: Olá!
+          </p>
+          <p>
+            <strong>João</strong>: Olár!
+          </p>
+        </MessagesContainer>
+        <InputsContainer>
+          <NameInput placeholder={"Nome"} />
+          <MessageInput placeholder={"Mensagem"} />
+          <button>Enviar</button>
+        </InputsContainer>
       </AppContainer>
     );
   }
