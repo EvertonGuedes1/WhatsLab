ed5cdfc5c593e15456cd5d8b967d937cb10b5958
Everton Guedes <evertonguedes1@gmail.com>
2021-12-20 13:01:07 -0300

    commit configuração inicial React

---
+++ whatslabb/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  border: 1px solid black;
+  height: 100vh;
+  box-sizing: border-box;
+  width: 600px;
+  margin: auto;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppContainer>
+        Olar Whatslabbbbbb
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
